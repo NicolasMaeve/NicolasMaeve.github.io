e64dcb38a9894137cc56135449be1344b51b6355
EduardoStarZ <pathver@gmail.com>
2024-01-20 16:31:47 -0300

    added the image modal library and implemented it

--- script.js
+++ script.js
@@ -27,15 +27,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
   
   // Add a click event on each of them
-  $navbarBurgers.forEach(el => {
-    el.addEventListener('click', () => {
+  $navbarBurgers.forEach(mobile_bar => {
+    mobile_bar.addEventListener('click', () => {
       
       // Get the target from the "data-target" attribute
-      const target = el.dataset.target;
+      const target = mobile_bar.dataset.target;
       const $target = document.getElementById(target);
       
       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      el.classList.toggle('is-active');
+      mobile_bar.classList.toggle('is-active');
       $target.classList.toggle('is-active');
       
     });
